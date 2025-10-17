ff67a2877648e52d01a5b8f06d792dcd262bda9d
tls123456798 <tlsgotjd1588@naver.com>
2025-10-17 10:49:09 +0900

    20251017
    플레이어 무브 스크립트 일부 작성

--- Assets/01.Scripts/PlayerMovement.cs
+++ Assets/01.Scripts/PlayerMovement.cs
@@ -12,11 +12,22 @@ public class PlayerMovement : MonoBehaviour {
 
     private void Start() {
         // 사용할 컴포넌트들의 참조를 가져오기
+        playerInput = GetComponent<PlayerInput>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
     private void FixedUpdate() {
         // 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
+
+        // 회전 실행
+        Rotate();
+        // 움직임 실행
+        Move();
+
+        // 입력값에 따라 애니메이터의 Move 파라미터값 변경
+        playerAnimator.SetFloat("Move", playerInput.move);
     }
 
     // 입력값에 따라 캐릭터를 앞뒤로 움직임

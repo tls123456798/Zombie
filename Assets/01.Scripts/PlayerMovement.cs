0af8472ff996029f805beabfdeaea062ef37bf8e
tls123456798 <tlsgotjd1588@naver.com>
2025-10-20 10:47:58 +0900

    20251020
    playermove 스크립트 완성

--- Assets/01.Scripts/PlayerMovement.cs
+++ Assets/01.Scripts/PlayerMovement.cs
@@ -32,11 +32,18 @@ public class PlayerMovement : MonoBehaviour {
 
     // 입력값에 따라 캐릭터를 앞뒤로 움직임
     private void Move() {
-
+        // 상대적으로 이동할 거리 계산
+        Vector3 moveDistance = playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
+        // 리지드바디로 게임 오브젝트 위치 변경
+        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance); 
     }
 
     // 입력값에 따라 캐릭터를 좌우로 회전
     private void Rotate() {
-
+        // 상대적으로 회전할 수치 계산
+        float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
+        // 사용자가 오른쪽 방향키를 누르면 시계방향으로 초당 180도 회전을 갖는다.
+        // 리지드바디를 이용해 게임 오브젝트 회전 변경
+        playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
     }
 }

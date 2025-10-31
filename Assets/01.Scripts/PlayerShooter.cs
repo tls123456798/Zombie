ce0ff3273de3f1a79621d3a9c9f88bef3cd7cd8d
tls123456798 <tlsgotjd1588@naver.com>
2025-10-31 10:49:47 +0900

    20251031
    gun shoot 완성

--- Assets/01.Scripts/PlayerShooter.cs
+++ Assets/01.Scripts/PlayerShooter.cs
@@ -58,6 +58,23 @@ public class PlayerShooter : MonoBehaviour {
 
     // 애니메이터의 IK 갱신
     private void OnAnimatorIK(int layerIndex) {
-        
+        // 총의 기준점 gunPivot을 3D 모델의 오른쪽 팔꿈치 위치로 이동
+        gunPivot.position = playerAnimator.GetIKHintPosition(AvatarIKHint.RightElbow);
+
+        // IK를 사용하여 왼손의 위치와 회전을 총의 왼쪽 손잡이에 맞춤
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f);
+
+        playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandMount.rotation);
+
+        // IK를 사용하여 오른손의 위치와 회전을 초의 오른쪽 손잡이에 맞춤
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
+
+        playerAnimator.SetIKPosition(AvatarIKGoal.RightHand, rightHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.RightHand, rightHandMount.rotation);
+
+
     }
 }

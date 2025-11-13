9027f99787228b5a639dc7adf4b3c85b13640403
tls123456798 <tlsgotjd1588@naver.com>
2025-11-13 10:50:06 +0900

    20251113
    CreatZombie 메서드 완성

--- Assets/01.Scripts/ZombieSpawner.cs
+++ Assets/01.Scripts/ZombieSpawner.cs
@@ -36,11 +36,46 @@ public class ZombieSpawner : MonoBehaviour {
 
     // 현재 웨이브에 맞춰 좀비들을 생성
     private void SpawnWave() {
+        // 웨이브 1 증가
+        wave++;
+
+        // 현재 웨이브 * 1.5에 반올림한 개수만큼 좀비 생성
+        int spawnCount = Mathf.RoundToInt(wave * 1.5f);
+
+        // spawnCount만큼 좀비 생성
+        for (int i = 0; i < spawnCount; i++)
+        {
+            // 좀비 생성 처리 실행
+            CreateZombie();
+        }
 
     }
 
     // 좀비를 생성하고 생성한 좀비에게 추적할 대상을 할당
     private void CreateZombie() {
+        // 사용할 좀비 데이터 랜덤으로 결정
+        ZombieData zombieData = zombieDatas[Random.Range(0, zombieDatas.Length)];
+
+        // 생성할 위치를 랜덤으로 결정
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        // 좀비 프리팹으로부터 좀비 생성
+        Zombie zombie = Instantiate(zombiePrefab, spawnPoint.position, spawnPoint.rotation);
+
+        // 생성할 좀비의 능력치 설정
+        zombie.Setup(zombieData);
+
+        // 생성된 좀비를 리스트에 추가
+        zombies.Add(zombie);
+
+        // 좀비의 onDeath 이벤트에 익명 메서드 등록
+        // 사망한 좀비를 리스트에서 제거
+        zombie.onDeath += () => zombies.Remove(zombie);
+        // 사망한 좀비를 10초 뒤에 파괴
+        zombie.onDeath += () => Destroy(zombie.gameObject, 10f);
+        // 좀비 사망 시 점수 상승
+        zombie.onDeath += () => GameManager.instance.AddScore(100);
+
 
     }
 }
